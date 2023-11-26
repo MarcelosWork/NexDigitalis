04cef8e15b0e577219fd5279543b999b4c015579
Marcelo Santos <marcelo.ms.santos@ctw.bmwgroup.com>
2023-11-26 23:08:48 +0000

    Build configs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca4f46432aeeb942"; }
+/******/ 		__webpack_require__.h = function() { return "07528e5aaca21089"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
